b3d10f0f52af48e38163617de89eee95350d38a9
9p15p <744007688@qq.com>
2019-10-29 20:49:45 +0800

    加上了TensorBoard的功能

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import argparse
 import os
 import time
-
+import numpy as np
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
@@ -13,11 +13,16 @@ from models import *
 from utils import format_time
 from utils import progress_bar
 
-# global parameters
-DEFAULT_LR = 0.1
-MATCHING_EPOCHES = 200
-TRAIN_BATCH_SIZE = 128
-TEST_BATCH_SIZE = 100
+from tensorboardutils import matplotlib_imshow
+from tensorboardutils import select_n_random
+from tensorboardutils import plot_classes_preds
+from tensorboardutils import add_pr_curve_tensorboard
+
+from const_params import *
+
+import warnings
+warnings.filterwarnings("ignore")
+
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=DEFAULT_LR, type=float, help='learning rate')
@@ -48,15 +53,15 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=TRAIN_BATCH_SIZE,
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=TEST_BATCH_SIZE, shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-# net = ResNet18()
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
-net = densenet_cifar()
+# net = densenet_cifar()
 # net = ResNeXt29_2x64d()
 # net = MobileNet()
 # net = MobileNetV2()
@@ -93,6 +98,37 @@ criterion = nn.CrossEntropyLoss().to(device)
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=10)
 
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# create grid of images
+img_grid = torchvision.utils.make_grid(images)
+
+# show images
+# matplotlib_imshow(img_grid, one_channel=False)
+
+img_grid_unnorm = img_grid /2 +0.5
+
+# write to tensorboard
+writer.add_image('four_fashion_mnist_images', img_grid_unnorm)
+
+writer.add_graph(net, images)   #TODO:不知道为什么这个net在TensorBoard中是横着的。
+writer.close()
+
+# select random images and their target indices
+images, labels = select_n_random(trainset.data, trainset.targets)
+
+# get the class labels for each image
+class_labels = [classes[lab] for lab in labels]
+
+# log embeddings
+features = images.reshape(-1, 32 * 32 * 3)
+writer.add_embedding(features,
+                    metadata=class_labels,
+                    label_img=torch.transpose(images,1,3))
+writer.close()
+
 # Training
 def train(epoch):
     print('\nEpoch: %d' % epoch)
@@ -115,6 +151,19 @@ def train(epoch):
 
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
             % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if batch_idx % 100 == 99:  # every 100 mini-batches...
+
+            # ...log the running loss
+            writer.add_scalar('training loss',
+                              train_loss / 100,
+                              epoch * len(trainloader) + batch_idx)
+
+            # ...log a Matplotlib Figure showing the model's predictions on a
+            # random mini-batch
+            writer.add_figure('predictions vs. actuals',
+                              plot_classes_preds(net, inputs, targets,classes),
+                              global_step=epoch * len(trainloader) + batch_idx)
+
 
 def test(epoch):
     global best_acc
@@ -122,6 +171,12 @@ def test(epoch):
     test_loss = 0
     correct = 0
     total = 0
+    # 1. gets the probability predictions in a test_size x num_classes Tensor
+    # 2. gets the preds in a test_size Tensor
+    # takes ~10 seconds to run
+    class_probs = []
+    class_preds = []
+
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
@@ -129,12 +184,23 @@ def test(epoch):
             loss = criterion(outputs, targets)
 
             test_loss += loss.item()
+
             _, predicted = outputs.max(1)
+            probability =  [F.softmax(el, dim=0) for el in outputs]
+            class_probs.append(probability)
+            class_preds.append(predicted)
+
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-
             progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                 % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    test_probs = torch.cat([torch.stack(batch) for batch in class_probs])
+    test_preds = torch.cat(class_preds)
+
+    # plot all the pr curves
+    for i in range(len(classes)):
+        add_pr_curve_tensorboard(i, test_probs, test_preds,classes)
 
     # Save checkpoint.
     acc = 100.*correct/total
@@ -151,7 +217,7 @@ def test(epoch):
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
 
-
+print(net)
 timer1 = time.perf_counter()
 for epoch in range(start_epoch, 1 + start_epoch + MATCHING_EPOCHES):
     timer0 = time.process_time()
